21edbf2b2f4b5e7b584673ab8788041bdb936d22
neducod <nedunony16@gmail.com>
2026-05-02 19:13:33 +0100

    hero section background image

--- church-website/src/components/home/HeroSection.jsx
+++ church-website/src/components/home/HeroSection.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
 import React from 'react';
 
+import BackgroundVideo from '../assets/bgvideo.mp4'
+
 
 function HeroSection(){
     return(
         <>
+            <div className='relative w-full h-screen overflow-hidden'>
+                <video autoPlay loop muted playsInline className='absolute top-0 left-0 w-full h-full object-cover -z-10'>
+                    <source src={BackgroundVideo} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+                <div className='relative z[1] text-center text-amber-300'></div>
+            </div>
         </>
     )
 }
