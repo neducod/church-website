41dd9811e5f47de0fe5d099530aaf8c1c0ed3122
neducod <nedunony16@gmail.com>
2026-05-02 14:26:20 +0100

    Footer section done

--- church-website/src/components/layout/Footer.jsx
+++ church-website/src/components/layout/Footer.jsx
@@ -23,12 +23,12 @@ import { MdOutlineEmail } from "react-icons/md";
 function Footer(){
     return(
         <>
-            <footer className='mt-80 bg-[#141516] flex flex-col gap-6'>
-                <div>
+            <footer className='mt-80 bg-[#141516] flex flex-col gap-10 p-6'>
+                <div className='flex gap-2 flex-col'>
                     <LuChurch color='white' size='2rem' />
-                    <h6 className='text-[#a1a1a2] font-bold'>PRAISE HILL.</h6>
-                    <p className='text-[#a1a1a2]'>Raising lives, building faith, transforming destinies through Christ.</p>
-                    <div className='flex flex-row gap-2'>
+                    <h6 className='text-[#a1a1a2] font-bold text-xl'>PRAISE HILL.</h6>
+                    <p className='text-[#a1a1a2] text-lg'>Raising lives, building faith, transforming destinies through Christ.</p>
+                    <div className='flex flex-row gap-4'>
                         <FaFacebook color='white' size='2rem' />
                         <LuYoutube color='white' size='2rem' />
                         <FaWhatsapp color='white' size='2rem' />
@@ -36,57 +36,58 @@ function Footer(){
                     </div>
                 </div>
                 <div>
-                    <ul>
-                        <li className='text-[#a1a1a2]'>Home</li>
-                        <li className='text-[#a1a1a2]'>About Us</li>
-                        <li className='text-[#a1a1a2]'>Ministries</li>
-                        <li className='text-[#a1a1a2]'>Sermons</li>
-                        <li className='text-[#a1a1a2]'>Events</li>
-                        <li className='text-[#a1a1a2]'>Contact</li>
-                        <li className='text-[#a1a1a2]'>Give Online</li>
+                    <ul className='flex flex-col'>
+                        <li className='text-[#a1a1a2] text-lg'>Home</li>
+                        <li className='text-[#a1a1a2] text-lg'>About Us</li>
+                        <li className='text-[#a1a1a2] text-lg'>Ministries</li>
+                        <li className='text-[#a1a1a2] text-lg'>Sermons</li>
+                        <li className='text-[#a1a1a2] text-lg'>Events</li>
+                        <li className='text-[#a1a1a2] text-lg'>Contact</li>
+                        <li className='text-[#a1a1a2] text-lg'>Give Online</li>
                     </ul>
                 </div>
-                <div>
-                    <h4 className='text-[#a1a1a2]'>Service Sundays</h4>
+                <div className='flex flex-col gap-6'>
+                    <h4 className='text-[#a1a1a2] text-xl'>Service Sundays</h4>
                     <div>
-                        <h5 className='text-[#a1a1a2]'>Sunday Worship</h5>
-                        <p className='text-[#a1a1a2]'>9:00 AM</p>
+                        <h5 className='text-[#a1a1a2] text-lg'>Sunday Worship</h5>
+                        <p className='text-[#a1a1a2] text-base'>9:00 AM</p>
                     </div>
                     <div>
-                        <h5 className='text-[#a1a1a2]'>Midweek Service</h5>
-                        <p className='text-[#a1a1a2]'>Wednesday - 5:00PM</p>
+                        <h5 className='text-[#a1a1a2] text-lg'>Midweek Service</h5>
+                        <p className='text-[#a1a1a2] text-base'>Wednesday - 5:00PM</p>
                     </div>
                     <div>
-                        <h5 className='text-[#a1a1a2]'>Prayer Meeting</h5>
-                        <p className='text-[#a1a1a2]'>Friday - 6:00PM</p>
+                        <h5 className='text-[#a1a1a2] text-lg'>Prayer Meeting</h5>
+                        <p className='text-[#a1a1a2] text-base'>Friday - 6:00PM</p>
                     </div>
                     <div>
-                        <h5 className='text-[#a1a1a2]'>Youth Fellowship</h5>
-                        <p className='text-[#a1a1a2]'>Saturday - 4:00PM</p>
+                        <h5 className='text-[#a1a1a2] text-lg'>Youth Fellowship</h5>
+                        <p className='text-[#a1a1a2] text-base'>Saturday - 4:00PM</p>
                     </div>
                 </div>
-                <div>
-                    <div>
+                <div className='flex flex-col gap-6'>
+                    <div className='flex flex-row gap-2'>
                         < IoLocationOutline color='white' size='2rem' />
-                        <p className='text-[#a1a1a2]'>12 Corner Road</p>
+                        <p className='text-[#a1a1a2] text-lg'>12 Corner Road</p>
                     </div>
-                    <div>
+                    <div className='flex flex-row gap-2'>
                         <FaPhoneAlt color='white' size='2rem'/>
-                        <p className='text-[#a1a1a2]'>123 456 789</p>
+                        <p className='text-[#a1a1a2] text-lg'>123 456 789</p>
                     </div>
-                    <div>
+                    <div className='flex flex-row gap-2'>
                         <MdOutlineEmail color='white' size='2rem'/>
-                        <p className='text-[#a1a1a2]'>sendyourprayers@gmail.com</p>
+                        <p className='text-[#a1a1a2] text-lg'>sendyourprayers@gmail.com</p>
                     </div>
                     {/* MINI GOOGLE MAP */}
                 </div>
-                <div className='h-1 bg-green-400 w-full'></div>
-                <div>
+                <div className='h-[3px] bg-green-400 w-full'></div>
+                <div className='flex flex-col gap-8'>
                     <div>
-                        <p className='text-[#a1a1a2]'>@2026 Praise Hill. All rights reserved</p>
+                        <p className='text-[#a1a1a2] text-lg'>@2026 Praise Hill. All rights reserved</p>
                     </div>
-                    <div>
-                        <p className='text-[#a1a1a2]'>Designed and inspired by Josephine</p>
+                    {/* <div className='h-[3px] bg-green-400 w-full'></div> */}
+                    <div className='flex justify-center items-center p-4'>
+                        <p className='text-[#a1a1a2] text-sm'>Designed and inspired by Josephine</p>
                     </div>
                 </div>
             </footer>
