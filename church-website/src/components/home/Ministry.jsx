d0cebb4fd4f0be7ebaded2ad4d59ea90305f1a4a
neducod <nedunony16@gmail.com>
2026-05-03 05:37:51 +0100

    Ministry section array part one done

--- church-website/src/components/home/Ministry.jsx
+++ church-website/src/components/home/Ministry.jsx
@@ -1,5 +1,51 @@
 import { useState } from 'react'
 
+import ImageOne from '../assets/youthm.jpeg' 
+import ImageTwo from '../assets/outreach.jpeg'
+import ImageThree from '../assets/womenFellowship.jpeg'
+import ImageFour from '../assets/choir.jpeg'
+import ImageFive from '../assets/childenChurch.jpeg'
+import ImageSix from '../assets/menfellowship.jpeg'
+
+const data = [
+    {
+        id: 1,
+        title: "Children Ministry",
+        description: "jd",
+        image: ImageFive
+    },
+    {
+        id: 1,
+        title: "Youth Ministry",
+        description: "jd",
+        image: ImageOne
+    },
+    {
+        id: 1,
+        title: "Women Fellowship",
+        description: "jd",
+        image: ImageThree
+    },
+    {
+        id: 1,
+        title: "Men Fellowship",
+        description: "jd",
+        image: ImageSix
+    },
+    {
+        id: 1,
+        title: "Choir",
+        description: "jd",
+        image: ImageFour
+    },
+    {
+        id: 1,
+        title: "Outreach",
+        description: "jd",
+        image: ImageTwo
+    }
+]
+
 function Ministry(){
     return(
         <>
