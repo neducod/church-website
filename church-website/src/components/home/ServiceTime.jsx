bb51095e64801938706c73aa0aec41a127aaf4e6
neducod <nedunony16@gmail.com>
2026-05-03 05:10:26 +0100

    placed array in the center

--- church-website/src/components/home/ServiceTime.jsx
+++ church-website/src/components/home/ServiceTime.jsx
@@ -38,20 +38,20 @@ const data = [
 function ServiceTime(){
     return(
         <>
-        <div className='bg-white border border-slate-200 grid grid-cols-6 gap-2 rounded-xl p-2 text-sm'>
+        {/* <div className='bg-white border border-slate-200 grid grid-cols-6 gap-2 rounded-xl p-2 text-sm'>
             <h1 className='text-center text-slate-200 text-xl font-bold col-span-6'>Our Service Time</h1>
             <div className='bg-white border border-slate-200 grid grid-cols-6 gap-2 rounded-xl p-2 text-sm'>
                 
             </div>
             <div className='bg-slate-100 text-slate-600 h-28 placeholder:text-slate-600 placeholder:opacity-50 border border-slate-200 col-span-6 resize-none outline-none rounded-lg p-2 duration-300 focus:border-slate-600'></div>
-        </div>
+        </div> */}
 
 
-        <div className="flex flex-wrap gap-6 justify-center items-center">
+        <div className="flex flex-wrap gap-8 justify-center items-center mt-30">
             {data.map((item) => (
                 <div 
                     key={item.id}
-                    className='w-64 bg-white rounded-2xl shadow-md p-4 hover:shadow-xl transition'
+                    className='w-40 bg-white rounded-xl shadow-md p-4 hover:shadow-xl transition'
                 >
                 <img 
                     src={item.image}
