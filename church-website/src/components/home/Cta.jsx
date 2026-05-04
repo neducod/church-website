5c4246977c2eebf246a6e51a6a90e1011aea3c6b
neducod <nedunony16@gmail.com>
2026-05-04 22:52:56 +0100

    h one text

--- church-website/src/components/home/Cta.jsx
+++ church-website/src/components/home/Cta.jsx
@@ -6,7 +6,7 @@ function Cta(){
         <>
             <div>
                 <div>
-                    <h2>Support the work of God</h2>
+                    <h2 className='text-3xl font-extralight'>SUPPORT THE WORK OF GOD </h2>
                 </div>
                 <button className='inline-flex cursor-pointer items-center gap-1 rounded border border-slate-300 bg-gradient-to-b from-slate-50 to-slate-200 px-4 py-2 font-semibold hover:opacity-90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-300 focus-visible:ring-offset-2 active:opacity-100'>Give Now</button>
             </div>
