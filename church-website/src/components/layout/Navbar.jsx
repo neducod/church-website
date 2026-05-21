69f463a89cfb5269b66aacfa71e0bc44539ff7c7
neducod <nedunony16@gmail.com>
2026-05-21 03:54:01 +0100

    still need to fix the overall

--- church-website/src/components/layout/Navbar.jsx
+++ church-website/src/components/layout/Navbar.jsx
@@ -9,34 +9,27 @@ import {  LuX } from 'react-icons/lu';
 function Navbar(){
     const [isOpen, setIsOpen] = useState(false);
 
-    const navLinks = [
-        { name: 'Home', href: '#' },
-        { name: 'About', href: '#' },
-        { name: 'Services', href: '#' },
-        { name: 'Contact', href: '#' },
-    ];
 
+  const navLinks = [
+    { name: 'Home', href: '#' },
+    { name: 'About', href: '#' },
+    { name: 'Services', href: '#' },
+    { name: 'Contact', href: '#' },
+  ];
 
     return(
         <>
-          
-
-
-
-
-
+  
 
 
 
     <nav className="fixed top-0 left-0 w-full z-50 border-b border-white/5 bg-[#141516]/80 backdrop-blur-md px-6 py-4 md:px-12">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         
-        {/* Logo Section */}
         <h1 className="text-white font-bold text-xl tracking-tighter">
           PRAISE HILL<span className="text-blue-500">.</span>
         </h1>
 
-        {/* Desktop Navigation */}
         <div className="hidden md:flex items-center gap-8">
           {navLinks.map((link) => (
             <a
@@ -52,7 +45,6 @@ function Navbar(){
           </button>
         </div>
 
-        {/* Mobile Toggle */}
         <button 
           className="md:hidden text-white p-2"
           onClick={() => setIsOpen(!isOpen)}
@@ -61,9 +53,8 @@ function Navbar(){
         </button>
       </div>
 
-      {/* Mobile Menu Overlay inert-0 */}
       <div className={`
-        fixed inert-0 top-[72px] bg-[#141516] z-40 flex flex-col p-8 gap-6 transition-transform duration-300 md:hidden
+        fixed inset-0 top-[72px]  bg-[#141516] z-40 flex flex-col p-8 gap-0 transition-transform duration-300 md:hidden items-center justfiy-center
         ${isOpen ? 'translate-x-0' : 'translate-x-full'}
       `}>
         {navLinks.map((link) => (
@@ -77,18 +68,7 @@ function Navbar(){
           </a>
         ))}
       </div>
-    </nav>
-
-
-
-
-
-
-
-
-
-
-
+    </nav> 
 
 
 
